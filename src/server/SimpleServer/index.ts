4c1ec80bcf65f523ad0281dabebbca175c9ed75d
Himanshu <iamhbkulkarni@gmail.com>
2022-06-05 01:16:51 +0530

    Minor changes - Make class variables private - Add listening debug message

--- src/server/SimpleServer/index.ts
+++ src/server/SimpleServer/index.ts
@@ -3,9 +3,9 @@ import * as http from "http";
 import { ServerConfig } from "./types";
 
 class SimpleServer {
-  server: http.Server | undefined;
-  app: Application | undefined;
-  config: ServerConfig;
+  private server: http.Server | undefined;
+  private app: Application | undefined;
+  private config: ServerConfig;
   constructor(config: ServerConfig) {
     this.config = config;
   }
@@ -22,7 +22,9 @@ class SimpleServer {
 
     // create http server
     this.server = http.createServer(this.app);
-
+    this.server.addListener("listening", () =>
+      console.info(`Started listening at port ${this.config.port}`)
+    );
     this.server.listen();
   }
 }
